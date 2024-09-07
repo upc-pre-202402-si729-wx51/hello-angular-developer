fcbed7048a969a3c3aa1bd19f2482f5ed7d2d329
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-06 19:45:51 -0500

    feat(welcoming): added register developer view

---
+++ src/app/welcoming/components/register-developer/register-developer.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-register-developer',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './register-developer.component.html',
+  styleUrl: './register-developer.component.css'
+})
+export class RegisterDeveloperComponent {
+  developerForm = new FormGroup(
+    {
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+    }
+  );
+
+  @Output()
+  developerRegistered = new EventEmitter<{firstName: string, lastName: string}>();
+
+  myOnSubmit() {
+    let firstName = this.developerForm.value.firstName?.toString() ?? '';
+    let lastName = this.developerForm.value.lastName?.toString() ?? '';
+
+    this.onDeveloperRegistered(firstName, lastName);
+  }
+
+  onDeveloperRegistered(firstName: string, lastName: string) {
+   this.developerRegistered.emit({firstName, lastName});
+  }
+}
